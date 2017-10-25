b7a2c0afc8e6602c7c1b56d91e2c9bc681e4352c
Alfonso Perez <alfon@ratslap.com>
2017-10-25 08:20:15 -0700

    try without a source

--- webcams2/public/webcams.js
+++ webcams2/public/webcams.js
@@ -8,16 +8,18 @@ var execute = function() {
   fetch(URL_SERVER).then(res => res.json()).then(res => {
     var zones = res.zones.sf;
     var video = document.createElement("video");
-    var source = document.createElement("source");
+    // var source = document.createElement("source");
     video.id = "video-element";
     video["data-setup"] = '{"fluid": true,"playbackRates": [1, 1.5, 2] }';
     video.class = "video-js vjs-default-skin";
     video.poster = "//vjs.zencdn.net/v/oceans.png";
-    video.controls = false;
-    video.preload = "auto";
-    source.src = zones[0];
-    source.type = "application/x-mpegURL";
-    video.appendChild(source);
+    video.controls = true;
+    video.autoplay = true;
+    video.src = zones[0];
+    // video.preload = "auto";
+    // source.src = zones[0];
+    // source.type = "application/x-mpegURL";
+    // video.appendChild(source);
     container.appendChild(video);
 
     let player = videojs("video-element");
